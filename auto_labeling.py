e49ac3fe23674c6bce3fb02e08c18fe952e33948
dostawca_seksu <tdfvfhdtcdh251@gmail.com>
2025-08-12 00:47:23 +0300

    Update auto_labeling.py

--- auto_labeling.py
+++ auto_labeling.py
@@ -35,10 +35,39 @@ def main():
 
 def label_sentiment(message):
     NEGATIVE_KEYWORDS = {
-        'report', 'noob', 'idiot', 'end', 'ff', 'stupid', 'trash', 'cyka', 'blyat',
-        'репорт', 'нуб', 'рак', 'дно', 'мусор', 'даун', 'afk', 'retard', 'faggot', 'shut up',
-        'пидор', 'мразь', 'мать', 'ебал', 'сука', 'bitch', 'нахуй', 'fuck', 'fucked', 'family',
-        'мама', 'отец', 'батя', 'сын', 'son', 'father', 'mother', 'fu'
+    'noob', 'n00b', 'newb', 'newbie', 'idiot', 'moron', 'stupid', 'dumb', 'dumbass', 'retard',
+    'trash', 'garbage', 'shit', 'sh1t', 'crap', 'rubbish', 'loser', 'faggot', 'fag', 'gay',
+    'bitch', 'btch', 'whore', 'slut', 'asshole', 'ass', 'a55', 'douche', 'douchebag',
+    'fuck', 'fck', 'fuk', 'fucked', 'fucker', 'motherfucker', 'mf', 'shithead', 'dipshit',
+    'bastard', 'prick', 'dick', 'd1ck', 'cock', 'pussy', 'pu55y', 'twat', 'wanker',
+    'scrub', 'feeder', 'troll', 'troller', 'griefer', 'thrower', 'afk', 'afker', 'leaver',
+    'quit', 'report', 'rpt', 'rep', 'end', 'ff', 'suck', 'sucks', 'bad', 'terrible',
+    'awful', 'horrible', 'useless', 'worthless', 'dog', 'dogshit', 'ez', 'easy', 'noskill',
+    'braindead', 'brainless', 'r3tard', 'rtard', 'cancer', 'toxic', 'shutup', 'stfu', 'gtfo',
+    'kys', 'die', 'uninstall', 'lowskill', 'boosted', 'smurf', 'smurfer', 'ruin', 'ruiner',
+    'нуб', 'нубас', 'новичок', 'рак', 'раковый', 'дно', 'даун', 'дебил', 'идиот', 'тупой',
+    'глупый', 'тормоз', 'мудак', 'мусор', 'дерьмо', 'говно', 'ублюдок', 'чмо', 'чушок',
+    'гандон', 'засранец', 'долбаеб', 'долбоеб', 'бездарь', 'бездарный', 'нищий', 'бомж',
+    'лох', 'лошара', 'отсталый', 'тупая', 'тупое', 'кретин', 'урод', 'дебилоид', 'мразь',
+    'тварь', 'падла', 'падло', 'сволочь', 'придурок', 'дурак', 'дурачок', 'тупица',
+    'олень', 'осел', 'баран', 'козел', 'дебелый', 'тупорылый', 'тупоголовый', 'туполобый',
+    'дебильный', 'идиотский', 'рачина', 'рачище', 'шваль', 'отребье', 'позорник', 'позорный',
+    'хуй', 'хуе', 'хуйло', 'хуйня', 'хуета', 'похер', 'пох', 'похуй', 'нахуй', 'нахер',
+    'ебать', 'ебал', 'еби', 'ебаный', 'еблан', 'еба', 'ебарь', 'ебло', 'ебан', 'ебнутый',
+    'ебанутый', 'сука', 'сучка', 'пидор', 'пидорас', 'пидр', 'похер', 'хуесос', 'хуйсос',
+    'блядь', 'бля', 'блять', 'бляха', 'еблище', 'ебучка', 'похерист', 'хуяк', 'хуйпох',
+    'похуист', 'ебизм', 'ебанатик', 'ебанашка', 'хуйлуша', 'похаб', 'похабный', 'хуевый',
+    'хуёво', 'хуйовый', 'ебливый', 'ебанство', 'хуйство', 'пидорство', 'похуизм',
+    'репорт', 'рпт', 'реп', 'слив', 'сливщик', 'фидер', 'корм', 'афк', 'афка', 'ливер',
+    'свалил', 'конец', 'фф', 'похеризм', 'раки', 'нубы', 'дауны', 'фидера', 'ливеры',
+    'афкеры', 'афкашник', 'слива', 'кормило', 'кормильщик', 'токсик', 'токсичный',
+    'мамка', 'мама', 'мать', 'батя', 'отец', 'сын', 'дочь', 'мамку', 'матери', 'папаша',
+    'сынок', 'дочурка', 'матюгальник',
+    'cyka', 'suka', 'blyat', 'blya', 'blyad', 'pidor', 'pidaras', 'pidr', 'mudak', 'debil',
+    'rak', 'dno', 'chmo', 'gandon', 'ebal', 'ebat', 'eban', 'ebanutiy', 'naxuy', 'naxer',
+    'poxer', 'poxuy', 'xuy', 'xuj', 'xui', 'xyi', 'xyilo', 'xuylo', 'sosi', 'sosy',
+    'lowbob', 'lowbobik', 'noobik', 'tupitsa', 'durak', 'durachok', 'xuesos', 'xuylo',
+    'eblan', 'eblishe', 'suka', 'blyadina', 'poh', 'pohab', 'xuyak', 'ebaka', 'ebanashka'
     }
 
     POSITIVE_KEYWORDS = {
